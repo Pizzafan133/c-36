6b31219500416e9bc90c39889f2a48e1fbfe3bc0
Pizzafan133 <76629426+Pizzafan133@users.noreply.github.com>
2021-04-07 14:00:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock,Feed;
+var addFood;
+var foodObj;
+
+//create feed and lastFed variable here
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+
+  addFood=createButton("Add Food");
+  addFood.position(800,205);
+  addFood.mousePressed(addFoods);
+
+  Feed=createButton("Feed The Dog");
+  Feed.position(700,205);
+  Feed.mousePressed(feedDog);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  
+ 
+  //write code to display text lastFed time here
+
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+ var food_stock_val = foodObj.getFoodStock();
+ if(food_stock_val <=0){
+  foodObj.updateFoodStock(food_stock_val*0)
+ }
+else{
+  foodObj.updateFoodStock(food_stock_val-1)
+  foodObj.getHours()
+  if(lastFed>=12){
+   
+  }
+  else if (lastFed===0){
+    text("last feed : 12AM",350,30)
+  }
+  else{
+    
+
+  }
+  }
+}
+
+
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
